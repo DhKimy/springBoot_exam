09eb2c5a473c737dc7dd7e56c634ee2d961b824e
dhk767 <dhk767@gmail.com>
2022-08-11 14:39:37 +0900

    add TestCase(question truncate)

--- src/main/java/com/ll/exam/sbb/repository/QuestionRepository.java
+++ src/main/java/com/ll/exam/sbb/repository/QuestionRepository.java
@@ -2,6 +2,9 @@ package com.ll.exam.sbb.repository;
 
 import com.ll.exam.sbb.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,4 +15,9 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubjectAndContent(String subject, String content);
 
     List<Question> findBySubjectLike(String subject);
+
+    @Transactional
+    @Modifying
+    @Query(value = "truncate question", nativeQuery = true)
+    void truncate();
 }
--- src/test/java/com/ll/exam/sbb/SbbApplicationTests.java
+++ src/test/java/com/ll/exam/sbb/SbbApplicationTests.java
@@ -24,24 +24,24 @@ class SbbApplicationTests {
 	}
 
 	@Test
-	void testJpa() {
+	void testJpa0() {
 		Question q1 = new Question();
 		q1.setSubject("sbb가 무엇인가요?");
 		q1.setContent("sbb에 대해서 알고 싶습니다.");
 		q1.setCreateDate(LocalDateTime.now());
-		questionRepository.save(q1);  // 첫번째 질문 저장
+		questionRepository.save(q1);
 
 		Question q2 = new Question();
 		q2.setSubject("스프링부트 모델 질문입니다.");
 		q2.setContent("id는 자동으로 생성되나요?");
 		q2.setCreateDate(LocalDateTime.now());
-		questionRepository.save(q2);  // 두번째 질문 저장
+		questionRepository.save(q2);
 
-		assertThat(q1.getId()).isGreaterThan(0);
-		assertThat(q2.getId()).isGreaterThan(q1.getId());
+		questionRepository.truncate();
 	}
 
-	@Test
+
+		@Test
 	void testJpa2() {
 		List<Question> all = questionRepository.findAll();
 		assertEquals(2, all.size());
@@ -57,14 +57,14 @@ class SbbApplicationTests {
 
 	@Test
 	void testJpa4() {
-		Question q = this.questionRepository.findBySubjectAndContent(
+		Question q = questionRepository.findBySubjectAndContent(
 				"sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
 		assertEquals(1, q.getId());
 	}
 
 	@Test
 	void testJpa5() {
-		List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+		List<Question> qList = questionRepository.findBySubjectLike("sbb%");
 		Question q = qList.get(0);
 		assertEquals("sbb가 무엇인가요?", q.getSubject());
 	}
@@ -75,7 +75,7 @@ class SbbApplicationTests {
 		assertTrue(oq.isPresent());
 		Question q = oq.get();
 		q.setSubject("수정된 제목");
-		this.questionRepository.save(q); // UPDATE
+		questionRepository.save(q); // UPDATE
 	}
 
 	@Test
@@ -85,9 +85,9 @@ class SbbApplicationTests {
 		assertTrue(oq.isPresent());
 		Question q = oq.get();
 
-		this.questionRepository.delete(q);
+		questionRepository.delete(q);
 
-		assertEquals(1, this.questionRepository.count());
+		assertEquals(1, questionRepository.count());
 	}
 
 
